2b2cb4ea79c4c45f302430db2895ec42b41e9477
Alejandro Almaraz <aaxx1207p@gmail.com>
2020-10-09 03:00:09 -0500

    task to extract data from parquet format to postgrest

--- scripts_python/dispersion_postgres.py
+++ scripts_python/dispersion_postgres.py
@@ -1,16 +1,17 @@
 import pandas as pd
 import psycopg2
 import sqlalchemy
+import numpy as np
 
 def createTables():
 
-    conn = psycopg2.connect(host="localhost", database="conekta",port = 5432, user="airflow", password="airflow")
+    conn = psycopg2.connect(host="postgres", database="conekta",port = 5432, user="airflow", password="airflow")
     conn.autocommit = True
     cursor = conn.cursor()
     sqlCreateCompanies = """ 
     create table companies
     (
-        company_id varchar(24) not null
+        company_id varchar(100) not null
             constraint companies_pk
 			    primary key,
         company_name varchar(130) not null
@@ -55,10 +56,25 @@ def createTables():
     conn.close()
 
 
+def convertNone(string):
+    string = str(string)
+    if (string.strip() == "nan") or (string == "None"):
+         return "-"
+    else:
+        return string
+
+
+def changeCompanyName(string):
+    string = str(string)
+    if (string == "MiPas0xFFFF") or (string == "MiP0xFFFF") or ( string.startswith("MiP") ):
+         return "MiPasajefy"
+    else:
+        return string
+
 
 def applyEtlParquetToPostgres():
     
-    conn = psycopg2.connect(host="localhost", port = 5432, user="airflow", password="airflow")
+    conn = psycopg2.connect(host="postgres", port = 5432, user="airflow", password="airflow")
     conn.autocommit = True
     cursor = conn.cursor()
     sql = " SELECT 1 AS result FROM pg_database WHERE datname='conekta' "
@@ -73,22 +89,36 @@ def applyEtlParquetToPostgres():
     createTables()
 
     
-    df = pd.read_parquet("/home/alejandro/Documentos/projects/Conekta/Pipeline/mnt/airflow/dags/src/data/data_formatted.parquet")
+    df = pd.read_parquet("/usr/local/airflow/dags/src/data/data_formatted.parquet")
 
-    engine = sqlalchemy.create_engine("postgresql://airflow:airflow@localhost/conekta")
+    engine = sqlalchemy.create_engine("postgresql://airflow:airflow@postgres/conekta")
     con = engine.connect()
 
-    print(df)
+    df.rename(columns={"name": "company_name", "paid_at": "updated_at"}, inplace=True)
 
-    table_name = 'testing'
-    df.to_sql(table_name, con,if_exists='replace', index=False)
+    df_companies = df[["company_name","company_id"]].drop_duplicates()
 
-    print(engine.table_names())
+    df_companies["company_id"] = df_companies["company_id"].map(lambda x: convertNone(x))
+    df_companies["company_name"] = df_companies["company_name"].map(lambda x: convertNone(x))
+    df_companies["company_name"] = df_companies["company_name"].map(lambda x: changeCompanyName(x))
+    
+    df_transaccions = df[["id","amount","status", "created_at","updated_at","company_id"]]
 
+    df_companies = df_companies[~ (df_companies["company_id"] == "*******")]
+
+    df_companies = df_companies[((df_companies.company_name!="-") & (df_companies.company_id!="-"))].drop_duplicates()
+
+    table_companies = 'companies'
+    table_tran = 'transacciones'
+
+    dfCompanies = pd.read_sql("SELECT t.company_id FROM public.companies t", con).rename(columns={"company_id": "companyid"})
+
+
+    df_result = pd.merge(df_companies, dfCompanies , left_on='company_id', right_on='companyid', how="left")
+
+    df_companies = df_result[df_result.companyid.isnull()][["company_id","company_name"]]
+    
+    df_companies.to_sql(table_companies, con,if_exists='append', index=False)
+    df_transaccions.to_sql(table_tran, con,if_exists='replace', index=False)
 
     con.close()
-
-applyEtlParquetToPostgres()
-
-
-
